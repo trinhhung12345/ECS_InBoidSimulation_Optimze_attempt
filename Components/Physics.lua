16bf158082e9f3f2b9404e1f6f17c424673669ca
trinhhung12345 <wearingarmor12345@gmail.com>
2025-04-16 14:39:33 +0700

    remove the physics component because it is unecessary now

--- Components/Physics.lua
+++
@@ -1,9 +0,0 @@
-local Concord = require("libraries.concord")
-
-local Physics = Concord.component("physics", function(e, body, shape, fixture)
-    e.body = body
-    e.shape = shape
-    e.fixture = fixture
-end)
-
-return Physics
